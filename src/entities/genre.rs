c70788cde13958461b5cab0bad530004e990b4fc
YamiHeike <koscielniak.z@gmail.com>
2025-12-01 21:27:30 +0100

    chore: entities refactor

--- src/entities/genre.rs
+++ src/entities/genre.rs
@@ -3,6 +3,7 @@ use uuid::Uuid;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Genre {
+    #[serde(rename = "_id", skip_serializing_if = "Option::is_none")]
     pub id: Option<Uuid>,
     pub name: String,
 }
--- src/models.rs
+++
@@ -1,7 +0,0 @@
-pub mod actor;
-pub mod genre;
-pub mod movie;
-
-pub use actor::Actor;
-pub use genre::Genre;
-pub use movie::Movie;
